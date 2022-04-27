9634e0aca1124c080aabae96833a36bd5c1b8110
Alexis David Pizarro Abarca <PZadpizarro@ads.loc>
2022-04-27 13:02:44 -0600

    BuscadorAdded

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import Buscador from "./componentes/Buscador";
 
 class App extends Component {
 
+  state = {
+    termino : '',
+    imagenes : []
+  }
+
+  consultarApi = () => {
+    const termino = this.state.termino;
+    const url = `https://pixabay.com/api/?key=26944772-b77b13b8fb5af22c750dda077&q=${termino}&per_page=30`; //Direccion de busqueda
+    // muestra la direccion de busqueda en consola console.log(url);
+    //Leer el Json
+    fetch(url)
+      .then(respuesta => respuesta.json())
+      .then(resultado => this.setState({ imagenes : resultado.hits}))
+
+  }
+  //reacciona a lo que se escribe en el input y llena termino con ese valor
+  datosBusqueda = (termino) => {
+    this.setState({
+      termino
+    }, () => {
+      this.consultarApi();
+    })
+  }
+
   render() { 
     return (
       <div className="app container">
@@ -10,9 +34,10 @@ class App extends Component {
           <p className="lead text-center">Buscador de Imagenes</p>
   
           <Buscador
-            mensaje="Buscador..."
+            datosBusqueda={this.datosBusqueda}
           />
         </div>
+        
       </div>
     );
   }
--- src/componentes/Buscador.js
+++ src/componentes/Buscador.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 
 class Buscador extends Component {
+
     busquedaRef = React.createRef();
+    
     obtenerDatos = (e) => {
         e.preventDefault();
-        console.log(this.busquedaRef.current.value);
+        const termino = this.busquedaRef.current.value;
+        this.props.datosBusqueda(termino);
     }
+    
     render() { 
         return ( 
             <form onSubmit={this.obtenerDatos}>
                 <div className='row'>
+                    
                     <div className='form-group col-md-8'>
                         <input ref={this.busquedaRef} type="text" className='form-control form-control-lg'
                         placeholder='Busca tu imagen. Ejemplo Futbol'/>
